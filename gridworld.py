23a3340d2f4222af23be3e5ae924f1a186099dfe
Marko <1995marko@gmail.com>
2021-02-24 16:52:23 +0100

    Mostly copy paste showcase.py

--- gridworld.py
+++ gridworld.py
@@ -4,9 +4,7 @@ import ray
 from really import SampleManager
 from gridworlds import GridWorld
 import os
-from really.utils import (
-    dict_to_dict_of_datasets,
-)  # convenient function for you to create tensorflow datasets
+
 
 """
 Your task is to solve the provided Gridword with tabular Q learning!
@@ -22,11 +20,7 @@ Have fun!!!!
 class TabularQ(object):
     def __init__(self, h, w, action_space):
         self.action_space = action_space
-
-        ## # TODO:
-        self.h = h
-        self.w = w
-        self.q_vals = np.zeros((self.h, self.w, self.action_space))
+        self.q_values = np.random.random((action_space, h, w))
 
     def __call__(self, state):
         ## # TODO:
@@ -36,10 +30,10 @@ class TabularQ(object):
 
     # # TODO:
     def get_weights(self):
-        return self.q_vals
+        return None
 
     def set_weights(self, q_vals):
-        self.q_vals = q_vals
+        pass
 
     # what else do you need?
 
@@ -63,7 +57,7 @@ if __name__ == "__main__":
     kwargs = {
         "model": TabularQ,
         "environment": GridWorld,
-        "num_parallel": 4,
+        "num_parallel": 2,
         "total_steps": 100,
         "model_kwargs": model_kwargs
         # and more
@@ -73,56 +67,36 @@ if __name__ == "__main__":
     ray.init(log_to_driver=False)
     manager = SampleManager(**kwargs)
 
-    saving_path = os.path.join(os.getcwd(), "progress_gridworld")
-
-    buffer_size = 5000
-    test_steps = 1000
+    buffer_size = 500
+    test_steps = 50
     epochs = 20
-    sample_size = 1000
+    sample_size = 100
     optim_batch_size = 8
     saving_after = 5
 
+    # keys for replay buffer -> what you will need for optimization
     optim_keys = ["state", "action", "reward", "state_new", "not_done"]
 
+    # initialize buffer
     manager.initilize_buffer(buffer_size, optim_keys)
 
-    manager.initialize_aggregator(
-        path=saving_path,
-        saving_after=saving_after,
-        aggregator_keys=['loss', 'time_steps']
-    )
+    # initilize progress aggregator
+    #saving_path = os.getcwd() + "/progress_test"
+    #manager.initialize_aggregator(
+    #    path=saving_path, saving_after=5, aggregator_keys=["loss", "time_steps"]
+    #)
 
-    print("test before training: ")
-    manager.test(
-        max_steps=100,
-        test_episodes=10,
-        render=True,
-        do_print=True,
-        evaluation_measure="time_and_reward",
-    )
+    # initial testing:
+    #print("test before training: ")
+    #manager.test(test_steps, do_print=True)
 
+    # get initial agent
     agent = manager.get_agent()
 
     for e in range(epochs):
-
         print("collecting experience..")
+
+        # Gives you state action reward trajetories
         data = manager.get_data()
         manager.store_in_buffer(data)
-
-        sample_dict = manager.sample(sample_size)
-        print(f"collected data for: {sample_dict.keys()}")
-        data_dict = dict_to_dict_of_datasets(
-            sample_dict,
-            batch_size=optim_batch_size
-        )
-
-        print("optimizing...")
-
-        losses = [
-            np.mean(np.random.normal(size=(64, 100)), axis=0) for _ in range(1000)
-        ]
-
-
-
-
-    # do the rest!!!!
+        pass
