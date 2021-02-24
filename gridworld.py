0b3e5cdd12d9691098d4410fce1015cb0578dbc1
Marko <1995marko@gmail.com>
2021-02-24 17:43:14 +0100

    Call with q-values implemented

--- gridworld.py
+++ gridworld.py
@@ -20,12 +20,15 @@ Have fun!!!!
 class TabularQ(object):
     def __init__(self, h, w, action_space):
         self.action_space = action_space
+        # todo proper Initialization?
         self.q_values = np.random.random((h, w, action_space))
 
     def __call__(self, state):
         ## # TODO:
+        h = int(state[0][0])
+        w = int(state[0][1])
         output = {}
-        output["q_values"] = np.random.normal(size=(1, self.action_space))
+        output["q_values"] = [self.q_values[h, w, :]]
         return output
 
     # # TODO:
