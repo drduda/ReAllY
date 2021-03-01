4bd2c12d3a81c765ec2c384afcf80b5d70160954
Marko <1995marko@gmail.com>
2021-03-02 00:38:09 +0100

    Added sample and train for loop. RAISE ERROR

--- hw2.py
+++ hw2.py
@@ -22,7 +22,27 @@ class DQN(tf.Module):
 if __name__ == "__main__":
 
     model = DQN()
+
+    # initialize
+    ray.init(log_to_driver=False)
     manager = SampleManager(DQN, 'CartPole-v0',
                             num_parallel=5, total_steps=100)
 
-    pass
+    buffer_size = 2000
+    epochs = 10
+
+    # keys for replay buffer -> what you will need for optimization
+    optim_keys = ["state", "action", "reward", "state_new", "not_done"]
+
+    # initialize buffer
+    manager.initilize_buffer(buffer_size, optim_keys)
+
+    # get initial agent
+    agent = manager.get_agent()
+
+    for e in range(epochs):
+        print("collecting experience..")
+
+        # Gives you state action reward trajetories
+        data = manager.get_data()
+        manager.store_in_buffer(data)
