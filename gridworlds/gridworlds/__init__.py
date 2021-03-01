9a611621e3a1e1d77b185b694cbee2590ea00ca1
Marko <1995marko@gmail.com>
2021-03-02 00:48:13 +0100

    Delete gridworlds, its not needed for the task and it containts a lot of IDE data

--- gridworlds/gridworlds/__init__.py
+++
@@ -1,8 +0,0 @@
-from gym.envs.registration import register
-from gridworlds.envs.gridworld import GridWorld
-
-register(
-    id="gridworld-v0",
-    entry_point="gridworlds.envs:GridWorld",
-    max_episode_steps=100000,
-)
