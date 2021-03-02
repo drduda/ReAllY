893ae3a513ac726fce190955374bcdf189ea528e
ndettmer <ndettmer@uos.de>
2021-03-02 13:58:53 +0100

    init aggregator

--- hw2.py
+++ hw2.py
@@ -8,25 +8,21 @@ import os
 from datetime import datetime
 
 
-class DQN(tf.Module):
-    def __init__(self, action_space=2):
+class DQN(tf.keras.Model):
+    def __init__(self, action_space=2, input_shape=2):
         super().__init__()
         self.action_space = action_space
-        self.layers = [
-            Dense(16),
-            LeakyReLU(),
-            Dense(32),
-            LeakyReLU(),
-            Dense(32),
-            LeakyReLU(),
-            Dense(self.action_space, activation=None)
-        ]
+        self.d1 = Dense(16, activation=LeakyReLU())
+        self.d2 = Dense(32, activation=LeakyReLU())
+        self.d3 = Dense(32, activation=LeakyReLU())
+        self.dout = Dense(self.action_space, activation=None)
 
     def __call__(self, state):
         output = {}
-        q = state
-        for layer in self.layers:
-            q = layer(q)
+        hidden = self.d1(state)
+        hidden = self.d2(hidden)
+        hidden = self.d3(hidden)
+        q = self.dout(hidden)
         output["q_values"] = q
         return output
 
@@ -42,12 +38,20 @@ if __name__ == "__main__":
 
     buffer_size = 2000
     epochs = 10
+    saving_path = os.path.join(os.getcwd(), '/progress_hw2')
+    saving_after = 5
 
     # keys for replay buffer -> what you will need for optimization
     optim_keys = ["state", "action", "reward", "state_new", "not_done"]
 
     # initialize buffer
     manager.initilize_buffer(buffer_size, optim_keys)
+
+    manager.initialize_aggregator(
+        path=saving_path,
+        saving_after=saving_after,
+        aggregator_keys=['loss', 'time_steps']
+    )
 
     # get initial agent
     agent = manager.get_agent()
