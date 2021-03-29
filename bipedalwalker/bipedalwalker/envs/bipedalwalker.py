edb285d4e1fd488e9c373bd4e236eae8b4dffdc8
ndettmer <ndettmer@uni-osnabrueck.de>
2021-03-29 17:49:31 +0200

    wrote down some ideas

--- bipedalwalker/bipedalwalker/envs/bipedalwalker.py
+++ bipedalwalker/bipedalwalker/envs/bipedalwalker.py
@@ -3,4 +3,12 @@ from gym.envs.box2d.bipedal_walker import BipedalWalker
 
 
 class MyBipedalWalker(BipedalWalker):
-    pass
+
+    def __init__(self):
+        super().__init__()
+        # TODO: override self.action_space and self.observation_space
+
+    def step(self, action):
+        # TODO: work with new action_space and observation_space
+        # TODO: remember to not execute anything for the head joint
+        super().step(action)
